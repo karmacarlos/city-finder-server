f6105ecb53e82276afbe2a8ded2155239bfc4d0a
Carlos Gomez <carlosalbertogomez108@gmail.com>
2021-11-18 12:13:00 -0700

    Change syntax for import cors to require, change response from all ends points, remove cors from parameter

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 const app = express()
-// const cors = require('cors')
-import cors from 'cors'
+const cors = require('cors')
+// import cors from 'cors'
 app.use(cors())
 app.use(express.json())
 import dotenv from 'dotenv'
@@ -9,15 +9,10 @@ dotenv.config()
 import fetch from 'node-fetch'
 app.set('port', process.env.PORT || 3001);
 
-// const corsOptions = {
-//   origin: 'http://localhost:3000/',
-//   optionsSuccessStatus: 200
-// }
-
 const geoDBkey = process.env.GEODB_KEY;
 const walkScoreKey = process.env.WALKSCORE_KEY;
 
-app.get('/geoDB/:minPopulation', cors(), (request, response) => {
+app.get('/geoDB/:minPopulation', (request, response) => {
   const minPopulation = request.params.minPopulation
     fetch(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=20&countryIds=Q30&minPopulation=${minPopulation}`, {
             "method": "GET",
@@ -29,43 +24,25 @@ app.get('/geoDB/:minPopulation', cors(), (request, response) => {
    .then(externalResponse => {
     response.send(externalResponse)
   })
-  //  .then(externalResponse => {
-  //   if (!externalResponse.ok) {
-  //     throw new Error(`Status: ${externalResponse.status} message: ${externalResponse.statusText}`)
-  //   }
-  //   return externalResponse.json()
-  // })
-  // .then(data => response.status(200).json(data))
-  // .catch(error => response.send( { error: error } ))
 })
 
-app.get('/wiki/:fetchQuery', cors(), (request,response) => {
+app.get('/wiki/:fetchQuery', (request,response) => {
   const fetchQuery = request.params.fetchQuery
   fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${fetchQuery}`)
   .then(externalResponse => {
-    if (!externalResponse.ok) {
-      throw new Error(`Status: ${externalResponse.status} message: ${externalResponse.statusText}`)
-    }
-    return externalResponse.json()
+    response.send(externalResponse)
   })
-  .then(data => response.status(200).json(data))
-  .catch(error => response.send( { error } ))
 })
 
-app.get('/walkScores/:city/:state/:lat/:lon', cors(), (request, response) => {
+app.get('/walkScores/:city/:state/:lat/:lon', (request, response) => {
   const city = request.params.city
   const state = request.params.state
   const latitude = request.params.lat
   const longitude = request.params.lon
   fetch(`https://api.walkscore.com/score?format=json&address=${city}%20${state}&lat=${latitude}&lon=${longitude}&transit=1&bike=1&wsapikey=${walkScoreKey}`)
   .then(externalResponse => {
-    if (!externalResponse.ok) {
-      throw new Error(`Status: ${externalResponse.status} message: ${externalResponse.statusText}`)
-    }
-    return externalResponse.json()
+    response.send(externalResponse)
   })
-  .then(data => response.status(200).json(data))
-  .catch(error => response.send( { error } ))
 })
 
 app.listen(app.get('port'), () => {
